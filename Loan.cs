62ce4d88fd31055a81b11cc2cb1b74b0853f10fe
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-08-07 12:08:15 -0700

    changed input parameters of CalculateMonthlyMortgage to match the understanding of the user

--- Loan.cs
+++ Loan.cs
@@ -9,11 +9,11 @@ namespace MortgageCalculator
     public class Loan
     {
         // Loan class should accept loan amount, loan length in months, and monthly interest rate in a constructor
-        private readonly decimal _loanAmount;
-        private readonly int _loanLengthInMonths;
-        private readonly decimal _monthlyInterestRate;
+        private readonly double _loanAmount;
+        private readonly int _loanLengthInYears;
+        private readonly double _yearlyInterestRate;
 
-        public Loan(decimal loanAmount, int loanLengthInMonths, decimal monthlyInterestRate)
+        public Loan(double loanAmount, int loanLengthInYears, double yearlyInterestRate)
         {
             // handle the inputs
             if (loanAmount <= 0)
@@ -22,26 +22,29 @@ namespace MortgageCalculator
             }
             else { _loanAmount = loanAmount; }
 
-            if (loanLengthInMonths <= 0)
+            if (loanLengthInYears <= 0)
             {
-                throw new ArgumentException("The loan length in months must be greater than 0");
+                throw new ArgumentException("The loan length in years must be greater than 0");
             }
-            else { _loanLengthInMonths = loanLengthInMonths; }
+            else { _loanLengthInYears = loanLengthInYears; }
 
-            if (monthlyInterestRate < 0)
+            if (yearlyInterestRate < 0)
             {
-                throw new ArgumentException("The monthly interest rate cannot be negative");
+                throw new ArgumentException("The yearly interest rate cannot be negative");
             }
-            else { _monthlyInterestRate = monthlyInterestRate; }
+            else { _yearlyInterestRate = yearlyInterestRate; }
         }
 
         // method to calculate monthly payment for mortgage
-        public static decimal CalculateMonthlyMortgagePayment(decimal loanAmount, int loanLengthInMonths, decimal monthlyInterestRate)
+        public static double CalculateMonthlyMortgagePayment(double loanAmount, int loanLengthInYears, double yearlyInterestRate)
         {
+            // do the conversions
+            double monthlyInterestRate = yearlyInterestRate / 12 / 100; // convert to 0.05 format
+            int totalPayments = loanLengthInYears * 12;
+            
             // do the calculations
-            decimal numerator = loanAmount * (monthlyInterestRate / 1200);
-            decimal denominator = 1 - (1 + (monthlyInterestRate / 1200) * 60);
-            return Math.Abs(numerator / denominator);
+            double monthlyPayment = loanAmount * (monthlyInterestRate * Math.Pow(1 + monthlyInterestRate, totalPayments)) / (Math.Pow(1 + monthlyInterestRate, totalPayments) - 1);
+            return Math.Round(monthlyPayment, 2);
         }
     }
 }
